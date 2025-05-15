128a740fe4184ce49f3f30348312a0ffc1aace15
Moti-Sultan <motisultannur@gmail.com>
2025-05-15 10:22:21 +0200

    Création de la table Positions

--- Base_de_donnees/schema_sql/gestion_RH_bdd_schema.sql
+++ Base_de_donnees/schema_sql/gestion_RH_bdd_schema.sql
@@ -11,3 +11,12 @@ CREATE TABLE department (
     manager_id INT,
     CONSTRAINT fk_department_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id) ON DELETE SET NULL
 );
+
+-- Table position
+CREATE TABLE position (
+    position_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    department_id INT NOT NULL,
+    salary_range VARCHAR(50),
+    CONSTRAINT fk_position_department FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
+);
