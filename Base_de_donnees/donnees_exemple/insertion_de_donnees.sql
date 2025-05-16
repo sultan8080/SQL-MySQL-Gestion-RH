0147c49ee9902b23f99e03cb4a32062a9e801981
Moti-Sultan <motisultannur@gmail.com>
2025-05-16 23:34:12 +0200

    Ajout des données dans la table leave_status

--- Base_de_donnees/donnees_exemple/insertion_de_donnees.sql
+++ Base_de_donnees/donnees_exemple/insertion_de_donnees.sql
@@ -15,3 +15,10 @@ INSERT INTO leave_status (status_name) VALUES
 ('Rejeté');
 
 SELECT * FROM leave_status;
+
+-- Statuts de projet
+INSERT INTO project_status (status_name) VALUES
+('Non démarré'),
+('En cours'),
+('Terminé'),
+('En pause');
