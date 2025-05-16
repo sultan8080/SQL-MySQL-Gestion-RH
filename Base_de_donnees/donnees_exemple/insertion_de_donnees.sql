b6275dd4b573bed4de7e7c78655d104a647f03fc
Moti-Sultan <motisultannur@gmail.com>
2025-05-16 23:23:44 +0200

    Ajout des données dans la table attendance_status

---
+++ Base_de_donnees/donnees_exemple/insertion_de_donnees.sql
@@ -0,0 +1,10 @@
+-- Statuts de présence
+
+-- INSERT INTO attendance_status (status_name) VALUES
+-- ('Présent'),
+-- ('Absent'),
+-- ('En congé'),
+-- ('Télétravail');
+
+SELECT * FROM attendance_status;
+
