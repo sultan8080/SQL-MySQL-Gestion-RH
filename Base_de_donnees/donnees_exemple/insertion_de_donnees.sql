e535bec0dba89c661f85615d5cd27ae7508331bc
Moti-Sultan <motisultannur@gmail.com>
2025-05-16 23:32:40 +0200

    Ajout des données dans la table leave_status

--- Base_de_donnees/donnees_exemple/insertion_de_donnees.sql
+++ Base_de_donnees/donnees_exemple/insertion_de_donnees.sql
@@ -1,10 +1,17 @@
 -- Statuts de présence
 
--- INSERT INTO attendance_status (status_name) VALUES
--- ('Présent'),
--- ('Absent'),
--- ('En congé'),
--- ('Télétravail');
+INSERT INTO attendance_status (status_name) VALUES
+('Présent'),
+('Absent'),
+('En congé'),
+('Télétravail');
 
 SELECT * FROM attendance_status;
 
+-- Statuts de congé
+INSERT INTO leave_status (status_name) VALUES
+('En attente'),
+('Approuvé'),
+('Rejeté');
+
+SELECT * FROM leave_status;
