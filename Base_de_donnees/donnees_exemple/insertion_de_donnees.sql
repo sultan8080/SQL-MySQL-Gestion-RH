d1963562f40b73c8b4c4988808c2ab72574e3fc6
Moti-Sultan <motisultannur@gmail.com>
2025-05-16 23:42:39 +0200

    Ajout des données dans la table job_role

--- Base_de_donnees/donnees_exemple/insertion_de_donnees.sql
+++ Base_de_donnees/donnees_exemple/insertion_de_donnees.sql
@@ -6,15 +6,11 @@ INSERT INTO attendance_status (status_name) VALUES
 ('En congé'),
 ('Télétravail');
 
-SELECT * FROM attendance_status;
-
 -- Statuts de congé
 INSERT INTO leave_status (status_name) VALUES
 ('En attente'),
 ('Approuvé'),
 ('Rejeté');
-
-SELECT * FROM leave_status;
 
 -- Statuts de projet
 INSERT INTO project_status (status_name) VALUES
@@ -23,7 +19,6 @@ INSERT INTO project_status (status_name) VALUES
 ('Terminé'),
 ('En pause');
 
-SELECT * FROM project_status;
 
 INSERT INTO department (name, location) VALUES
 ('Ressources Humaines', 'Paris'),
@@ -36,3 +31,21 @@ INSERT INTO department (name, location) VALUES
 ('Développement Produit', 'Nantes'),
 ('Relations Publiques', 'Nice'),
 ('Qualité et Conformité', 'Grenoble');
+
+
+INSERT INTO job_role (title, department_id, min_salary, max_salary) VALUES
+('Développeur Web', 3, 35000, 70000),
+('Chef de Projet IT', 3, 45000, 90000),
+('Analyste Financier', 2, 40000, 85000),
+('Responsable Marketing', 4, 50000, 95000),
+('Spécialiste RH', 1, 38000, 72000),
+('Agent de Service Client', 6, 30000, 60000),
+('Logisticien', 7, 34000, 68000),
+('Responsable Relations Publiques', 9, 46000, 88000),
+('Contrôleur Qualité', 10, 42000, 82000),
+('Ingénieur Produit', 8, 48000, 92000),
+('Analyste des données', 3, 46000, 90000),
+('Consultant en stratégie', 2, 50000, 95000),
+('Gestionnaire de paie', 1, 38000, 75000),
+('Designer UX/UI', 3, 45000, 87000),
+('Chef de projet événementiel', 9, 44000, 86000);
