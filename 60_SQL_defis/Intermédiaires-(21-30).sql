5f4ff6179ef45c3344531a07e6e2bf92178a81ad
Moti-Sultan <motisultannur@gmail.com>
2025-05-17 22:08:47 +0200

    Q-22. Afficher les employés et leurs projets associés

--- 60_SQL_defis/Intermédiaires-(21-30).sql
+++ 60_SQL_defis/Intermédiaires-(21-30).sql
@@ -4,3 +4,10 @@ FROM employee e
 JOIN job_role j ON e.job_role_id = j.job_role_id
 WHERE title = "Chef de projet IT";
 
+
+-- 22. Afficher les employés et les projets auxquels ils sont affectés, s’il y en a.
+SELECT e.first_name, e.last_name, p.project_name
+FROM employee e
+LEFT JOIN project_assignment pa ON e.employee_id = pa.employee_id
+LEFT JOIN project p ON pa.project_id = p.project_id;
+
