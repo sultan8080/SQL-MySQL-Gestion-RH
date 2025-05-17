7e76b118a573d09d88214144e10cf4909e75d91e
Moti-Sultan <motisultannur@gmail.com>
2025-05-17 17:31:36 +0200

    Q-12. Afficher les projets et le nombre d’employés affectés

--- 60_SQL_defis/Intermédiaires-(11-40).sql
+++ 60_SQL_defis/Intermédiaires-(11-40).sql
@@ -5,3 +5,9 @@ JOIN job_role jr ON e.job_role_id = jr.job_role_id
 JOIN department d ON e.department_id = d.department_id;
 
 
+
+-- 12. Afficher les projets et le nombre d’employés affectés à chaque projet.
+SELECT p.project_name, COUNT(pa.employee_id) AS total_employes
+FROM project p
+LEFT JOIN project_assignment pa ON p.project_id = pa.project_id
+GROUP BY p.project_name;
