2a0f930cd8730ee30a4e29c157d785e37996cfed
Moti-Sultan <motisultannur@gmail.com>
2025-05-17 16:46:47 +0200

    rôles distincts dans job_role

--- 60_SQL_defis/1-10_debutant.sql
+++ 60_SQL_defis/1-10_debutant.sql
@@ -1,8 +1,7 @@
--- Défis Débutants (1–10) – Requêtes simples
+-- Défis Débutants (1–10)
 
 -- 1. Lister tous les employés avec leurs prénoms, noms et adresses e-mail.
 SELECT first_name, last_name, email FROM employee;
-
 
 
 -- 2. Afficher les noms des départements et leur localisation.
@@ -15,11 +14,16 @@ FROM employee e
 JOIN salary s ON e.employee_id = s.employee_id
 WHERE s.salary_amount > 70000;
 
+
 --4. Compter le nombre total d’employés dans la base de données.
 SELECT COUNT(*) AS total_employes FROM employee;
+
 
 --5. Trier les employés par date d’embauche (du plus récent au plus ancien).
 SELECT first_name, last_name, hire_date
 FROM employee
 ORDER BY hire_date DESC;
 
+
+--6. Afficher les rôles de poste distincts existants dans la table job_role.
+SELECT DISTINCT title FROM job_role;
