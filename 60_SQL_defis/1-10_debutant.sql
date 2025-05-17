f31eb9db1894ebab57a68d4b76107f3e0cf1711a
Moti-Sultan <motisultannur@gmail.com>
2025-05-17 16:41:04 +0200

    affichage des départements et leur localisation

--- 60_SQL_defis/1-10_debutant.sql
+++ 60_SQL_defis/1-10_debutant.sql
@@ -3,3 +3,11 @@
 -- 1. Lister tous les employés avec leurs prénoms, noms et adresses e-mail.
 
 SELECT first_name, last_name, email FROM employee;
+
+
+
+-- 2. Afficher les noms des départements et leur localisation.
+
+SELECT name, location FROM department;
+
+
