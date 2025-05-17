b09cd8c1a722cae8524b20ce951410fc0e2a84da
Moti-Sultan <motisultannur@gmail.com>
2025-05-17 16:43:54 +0200

    nombre total d’employés

--- 60_SQL_defis/1-10_debutant.sql
+++ 60_SQL_defis/1-10_debutant.sql
@@ -1,19 +1,20 @@
 -- Défis Débutants (1–10) – Requêtes simples
 
 -- 1. Lister tous les employés avec leurs prénoms, noms et adresses e-mail.
-
 SELECT first_name, last_name, email FROM employee;
 
 
 
 -- 2. Afficher les noms des départements et leur localisation.
-
 SELECT name, location FROM department;
 
 
 --3. Afficher le nom et le salaire de tous les employés dont le salaire est supérieur à 70 000.
-
 SELECT e.first_name, e.last_name, s.salary_amount
 FROM employee e
 JOIN salary s ON e.employee_id = s.employee_id
 WHERE s.salary_amount > 70000;
+
+--4. Compter le nombre total d’employés dans la base de données.
+
+SELECT COUNT(*) AS total_employes FROM employee;
