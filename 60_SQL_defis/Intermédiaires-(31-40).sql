5ad917c6adab0cde274559066aac93e237faebec
Moti-Sultan <motisultannur@gmail.com>
2025-05-18 10:57:21 +0200

    Q-32. Afficher le nombre de projets par employé (incluant 0)

--- 60_SQL_defis/Intermédiaires-(31-40).sql
+++ 60_SQL_defis/Intermédiaires-(31-40).sql
@@ -5,3 +5,15 @@ ORDER BY start_date ASC
 LIMIT 1;
 
 
+-- 32. Afficher les employés et leur nombre de projets, même ceux sans projet.
+SELECT e.first_name, e.last_name, COUNT(pa.project_id) AS total_projects
+FROM employee e
+LEFT JOIN project_assignment pa ON e.employee_id = pa.employee_id
+GROUP BY e.first_name, e.last_name;
+
+-- 32. Afficher les employés et leur nombre de projets, même ceux sans projet (afficher 0 si aucun).
+SELECT e.first_name, e.last_name, COALESCE(COUNT(pa.project_id), 0) AS total_projects
+FROM employee e
+LEFT JOIN project_assignment pa ON e.employee_id = pa.employee_id
+GROUP BY e.first_name, e.last_name;
+
