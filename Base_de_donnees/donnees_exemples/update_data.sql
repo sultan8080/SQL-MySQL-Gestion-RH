a932cda4eaf90ec7299c094cfb28cbf15ea24169
Moti-Sultan <motisultannur@gmail.com>
2025-05-18 13:15:38 +0200

    SQL-INSERT pour la table job_role chef de department

--- Base_de_donnees/donnees_exemples/update_data.sql
+++ Base_de_donnees/donnees_exemples/update_data.sql
@@ -22,4 +22,34 @@ SET manager_id = (
     LIMIT 1
 );
 
+-- department
+INSERT INTO department (name, location) VALUES
+('Ressources Humaines', 'Paris'),
+('Finance', 'Lyon'),
+('Informatique', 'Marseille'),
+('Marketing', 'Bordeaux'),
+('Gestion des Projets', 'Toulouse'),
+('Service Client', 'Strasbourg'),
+('Logistique', 'Lille'),
+('Développement Produit', 'Nantes'),
+('Relations Publiques', 'Nice'),
+('Qualité et Conformité', 'Grenoble');
+
+
 SELECT * FROM department;
+
+
+INSERT INTO job_role (title, department_id, min_salary, max_salary) VALUES
+('Responsable Ressources Humaines', 1, 45000, 90000),
+('Responsable Finance', 2, 48000, 92000),
+('Responsable Gestion des Projets', 5, 47000, 91000),
+('Responsable Service Client', 6, 43000, 85000),
+('Responsable Qualité et Conformité', 10, 44000, 86000),
+('Responsable logistique', 7, 40000, 70000),
+('Responsable Production', 8, 44000, 86000),
+('Responsable IT', 3, 45000, 90000),
+
+
+INSERT INTO job_role (title, department_id, min_salary, max_salary) VALUES
+('Responsable Marketing', 4, 50000, 95000),
+('Responsable Relations Publiques', 9, 46000, 88000),
