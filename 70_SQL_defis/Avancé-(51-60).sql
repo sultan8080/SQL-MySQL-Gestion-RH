d6562ba19a5003bdfbc56e6edf8a36856e06f716
Moti-Sultan <motisultannur@gmail.com>
2025-05-19 21:47:22 +0200

    Q-51. Durée moyenne des formations par formateur

--- 70_SQL_defis/Avancé-(51-60).sql
+++ 70_SQL_defis/Avancé-(51-60).sql
@@ -6,3 +6,19 @@ SELECT
 FROM training t
 JOIN trainer tr ON t.trainer_id = tr.trainer_id
 GROUP BY t.trainer_id, tr.name;
+
+
+-- 52. Compare les performances avant et après formation pour les employés ayant reçu au moins une formation.
+WITH training_dates AS (
+    SELECT et.employee_id, MIN(t.start_date) AS first_training_date
+    FROM employee_training et
+    JOIN training t ON et.training_id = t.training_id
+    GROUP BY et.employee_id
+)
+SELECT e.employee_id, 
+       AVG(CASE WHEN pr.review_date < td.first_training_date THEN pr.performance_score END) AS avg_before_training,
+       AVG(CASE WHEN pr.review_date >= td.first_training_date THEN pr.performance_score END) AS avg_after_training
+FROM employee e
+JOIN performance_review pr ON e.employee_id = pr.employee_id
+JOIN training_dates td ON e.employee_id = td.employee_id
+GROUP BY e.employee_id;
