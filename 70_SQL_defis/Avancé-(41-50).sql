5a0efeeea8172ef3c30c3bab33b36fa54c64520b
Moti-Sultan <motisultannur@gmail.com>
2025-05-19 12:58:13 +0200

    Q-43. Les employés les plus performants

--- 70_SQL_defis/Avancé-(41-50).sql
+++ 70_SQL_defis/Avancé-(41-50).sql
@@ -17,3 +17,31 @@ JOIN department d ON p.project_id = d.department_id
 GROUP BY d.name
 ORDER BY total_budget_used DESC;
 
+
+-- 43. Trouve l’employé ayant réalisé le plus grand nombre d’évaluations de performance.
+SELECT pr.reviewer_id, e.first_name, e.last_name, 
+       COUNT(pr.review_id) AS total_reviews
+FROM performance_review pr
+JOIN employee e ON pr.reviewer_id = e.employee_id
+GROUP BY pr.reviewer_id, e.first_name, e.last_name
+HAVING COUNT(pr.review_id) = (
+    SELECT MAX(review_count) 
+    FROM (
+        SELECT COUNT(review_id) AS review_count 
+        FROM performance_review 
+        GROUP BY reviewer_id
+    ) AS subquery
+);
+
+-- Alternative
+WITH review_counts AS (
+    SELECT reviewer_id, COUNT(review_id) AS total_reviews
+    FROM performance_review
+    GROUP BY reviewer_id
+)
+SELECT e.employee_id, e.first_name, e.last_name, rc.total_reviews
+FROM review_counts rc
+JOIN employee e ON rc.reviewer_id = e.employee_id
+WHERE rc.total_reviews = (SELECT MAX(total_reviews) FROM review_counts);
+
+
