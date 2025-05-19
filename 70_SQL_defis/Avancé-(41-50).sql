90b1ef0324f30f096fd3fddb591987c447a2b4d1
Moti-Sultan <motisultannur@gmail.com>
2025-05-19 12:31:09 +0200

    Q-42. Utilisation du budget par département

--- 70_SQL_defis/Avancé-(41-50).sql
+++ 70_SQL_defis/Avancé-(41-50).sql
@@ -10,3 +10,10 @@ HAVING COUNT(pa.assignment_id) > 1
 ORDER BY First_Project_start_date;
 
 
+-- 42. Calcule le budget total utilisé par chaque département via ses projets.
+SELECT d.name AS department_name, SUM(p.budget) AS total_budget_used
+FROM project p
+JOIN department d ON p.project_id = d.department_id
+GROUP BY d.name
+ORDER BY total_budget_used DESC;
+
